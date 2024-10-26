7fd6116380e26248d05e4bea5d745f7c3c063585
Bhavik Chauhan <38806989+bhavikchauhan93@users.noreply.github.com>
2024-10-26 07:52:54 +0000

    Enhanced prime factors code

--- my_code/my_factor.py
+++ my_code/my_factor.py
@@ -1,15 +1,29 @@
 def prime_factors(number):
   factors = []
   divisor = 2
+  prime_list = []
 
-  while divisor <= number:
+  #Find all primes
+  for num in range (2, number+1):
+    is_prime = True
+    for p in range (2, (num//2)+1):
+      if (num % p) == 0:
+        is_prime = False
+        break
+    if is_prime == True:  
+      prime_list.append(num)
+
+  #Using prime list find prime factors. 
+  while len(prime_list) != 0:
     if number % divisor == 0:
       factors.append(divisor)
       number = number // divisor
     else:
-        divisor += 1
+      divisor = prime_list.pop()
+
   return factors
   
 if __name__ == '__main__':
-    print(prime_factors(630))  # [2, 3, 3, 5, 7]
-    print(prime_factors(13))  # [13]
+    print(prime_factors(630))
+    print(prime_factors(13))
+    print(prime_factors(633))
