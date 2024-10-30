c471107fbd8c07aca7c8efffa19656085f1f51ed
bhavikchauhan93 <38806989+bhavikchauhan93@users.noreply.github.com>
2024-10-30 14:33:05 +0530

    my zipfile archive

---
+++ my_code/zip_archive/my_zip_archive.py
@@ -0,0 +1,17 @@
+from zipfile import ZipFile
+import os
+
+def zip_archive(path, extension_list, output_zip):
+  with ZipFile(output_zip, 'w') as outfile:
+    for dirpath, dirnames, filenames in os.walk(path):
+      #os.walk yields a 3-tuple (dirpath, dirnames, filenames).
+      #dirpath is a string, the path to the directory. 
+      #dirnames is a list of the names of the subdirectories in dirpath
+      #filenames is a list of the names of the non-directory files in dirpath
+      for file in filenames:
+        for type in extension_list:
+          if file.lower().endswith(type):
+            outfile.write(os.path.join(dirpath, file))
+      
+if __name__ == '__main__':
+  zip_archive('my_stuff',['.jpg','.txt'],'my_stuff_modified.zip')
