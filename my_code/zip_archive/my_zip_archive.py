661b3e7bb08d54cd63b00c8adc6e044b0324dd73
bhavikchauhan93 <38806989+bhavikchauhan93@users.noreply.github.com>
2024-10-30 14:40:13 +0530

    adding relative path incase zip_archive function is called using absolute path

--- my_code/zip_archive/my_zip_archive.py
+++ my_code/zip_archive/my_zip_archive.py
@@ -8,10 +8,11 @@ def zip_archive(path, extension_list, output_zip):
       #dirpath is a string, the path to the directory. 
       #dirnames is a list of the names of the subdirectories in dirpath
       #filenames is a list of the names of the non-directory files in dirpath
+      rel_path = os.path.relpath(dirpath, path)
       for file in filenames:
         for type in extension_list:
           if file.lower().endswith(type):
-            outfile.write(os.path.join(dirpath, file))
+            outfile.write(os.path.join(dirpath, file), arcname=os.path.join(rel_path, file))
       
 if __name__ == '__main__':
   zip_archive('my_stuff',['.jpg','.txt'],'my_stuff_modified.zip')
